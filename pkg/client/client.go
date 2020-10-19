49be20096dfb1353adfd71c4657eaa57696da587
ziran.iiu <ziran.liu@bhex.com>
2020-10-19 14:54:59 +0800

    First commit

---
+++ pkg/client/client.go
@@ -0,0 +1,82 @@
+package client
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hbtc-chain/gotron-sdk/pkg/proto/api"
+	"google.golang.org/grpc"
+)
+
+const grpcTimeout = 60 * time.Second
+
+// GrpcClient controller structure
+type GrpcClient struct {
+	Address string
+	Conn    *grpc.ClientConn
+	Client  api.WalletClient
+}
+
+// NewGrpcClient create grpc controller
+func NewGrpcClient(address string) *GrpcClient {
+	client := new(GrpcClient)
+	client.Address = address
+	return client
+}
+
+// Start initiate grpc  connection
+func (g *GrpcClient) Start() error {
+	var err error
+	if len(g.Address) == 0 {
+		g.Conn, err = grpc.Dial("",
+			grpc.WithInsecure(),
+			grpc.WithBalancer(grpc.RoundRobin(NewPseudoResolver(GetIPList()))),
+		)
+	} else {
+		g.Conn, err = grpc.Dial(g.Address, grpc.WithInsecure())
+	}
+	if err != nil {
+		return fmt.Errorf("Connecting GRPC Client: %v", err)
+	}
+	g.Client = api.NewWalletClient(g.Conn)
+	return nil
+}
+
+// Stop GRPC Connection
+func (g *GrpcClient) Stop() {
+	if g.Conn != nil {
+		g.Conn.Close()
+	}
+}
+
+// Reconnect GRPC
+func (g *GrpcClient) Reconnect(url string) error {
+	g.Stop()
+	if len(url) > 0 {
+		g.Address = url
+	}
+	g.Start()
+	return nil
+}
+
+// GetMessageBytes return grpc message from bytes
+func GetMessageBytes(m []byte) *api.BytesMessage {
+	message := new(api.BytesMessage)
+	message.Value = m
+	return message
+}
+
+// GetMessageNumber return grpc message number
+func GetMessageNumber(n int64) *api.NumberMessage {
+	message := new(api.NumberMessage)
+	message.Num = n
+	return message
+}
+
+// GetPaginatedMessage return grpc message number
+func GetPaginatedMessage(offset int64, limit int64) *api.PaginatedMessage {
+	return &api.PaginatedMessage{
+		Offset: offset,
+		Limit:  limit,
+	}
+}
